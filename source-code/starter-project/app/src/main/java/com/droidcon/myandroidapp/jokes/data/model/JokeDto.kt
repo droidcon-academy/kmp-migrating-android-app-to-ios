cb1dee67bc61d1a061ea3ec550d589494a5d3b00
ioannisa <ioannisanif@gmail.com>
2025-06-20 23:35:13 +0300

    package rename

--- source-code/starter-project/app/src/main/java/com/droidcon/myandroidapp/jokes/data/model/JokeDto.kt
+++
@@ -1,7 +0,0 @@
-package com.droidcon.myandroidapp.jokes.data.model
-
-data class JokeDto(
-    val id: Int,
-    val setup: String,
-    val punchline: String
-)
--- source-code/starter-project/app/src/main/java/com/droidcon/myandroidapp/jokes/data/network/JokesApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.droidcon.myandroidapp.jokes.data.network
-
-import com.droidcon.myandroidapp.jokes.data.model.JokeDto
-import retrofit2.http.GET
-
-interface JokesApiService {
-    @GET("/random_ten")
-    suspend fun getRandomJokes(): List<JokeDto>
-}
