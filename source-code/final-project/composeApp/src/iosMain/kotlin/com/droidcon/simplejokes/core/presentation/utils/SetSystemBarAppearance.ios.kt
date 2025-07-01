f54164d78cbee252cb70cf4c7de8aacd4cdc9d05
ioannisa <ioannisanif@gmail.com>
2025-07-01 12:03:52 +0300

    SetSystemBarAppearance ios

--- source-code/final-project/composeApp/src/iosMain/kotlin/com/droidcon/simplejokes/core/presentation/utils/SetSystemBarAppearance.ios.kt
+++ source-code/final-project/composeApp/src/iosMain/kotlin/com/droidcon/simplejokes/core/presentation/utils/SetSystemBarAppearance.ios.kt
@@ -3,6 +3,7 @@ package com.droidcon.simplejokes.core.presentation.utils
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import platform.UIKit.UIApplication
+import platform.UIKit.UIStatusBarStyleDarkContent
 import platform.UIKit.UIStatusBarStyleLightContent
 import platform.UIKit.setStatusBarStyle
 
@@ -10,8 +11,7 @@ import platform.UIKit.setStatusBarStyle
 actual fun SetSystemBarAppearance(darkTheme: Boolean) {
     LaunchedEffect(darkTheme) {
         UIApplication.sharedApplication.setStatusBarStyle(
-           // UIStatusBarStyleLightContent
-            //if (darkTheme) UIStatusBarStyleLightContent else UIStatusBarStyleDarkContent
+            if (darkTheme) UIStatusBarStyleLightContent else UIStatusBarStyleDarkContent
         )
     }
 }
