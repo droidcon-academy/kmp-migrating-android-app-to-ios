5d95116cbe98b8117e0258e32fea7fd5d4ca1c7f
ioannisa <ioannisanif@gmail.com>
2025-07-01 12:27:18 +0300

    Informative comments about StatusBars in iOS

--- source-code/final-project/composeApp/src/iosMain/kotlin/com/droidcon/simplejokes/core/presentation/utils/SetSystemBarAppearance.ios.kt
+++ source-code/final-project/composeApp/src/iosMain/kotlin/com/droidcon/simplejokes/core/presentation/utils/SetSystemBarAppearance.ios.kt
@@ -9,6 +9,12 @@ import platform.UIKit.setStatusBarStyle
 
 @Composable
 actual fun SetSystemBarAppearance(darkTheme: Boolean) {
+    // by default iOS doesn't need that code as it automatically changes UIStatusBar color
+    // based on the StatusBar background dimming.  Keep it in case you want to bypass default behaviour
+    // Note: Requires additional configuration in info.plist - out of scope
+
+    // alternatively you can just provide an empty function here as the code bellow gets ignored
+
     LaunchedEffect(darkTheme) {
         UIApplication.sharedApplication.setStatusBarStyle(
             if (darkTheme) UIStatusBarStyleLightContent else UIStatusBarStyleDarkContent
