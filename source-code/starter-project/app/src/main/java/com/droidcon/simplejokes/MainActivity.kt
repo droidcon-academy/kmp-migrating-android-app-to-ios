48d7e440940b12a15e08f35c3906eaaa92c1af36
ioannisa <ioannisanif@gmail.com>
2025-07-11 00:09:58 +0300

    New Initialization on Android Koin

--- source-code/starter-project/app/src/main/java/com/droidcon/simplejokes/MainActivity.kt
+++ source-code/starter-project/app/src/main/java/com/droidcon/simplejokes/MainActivity.kt
@@ -11,8 +11,17 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.droidcon.simplejokes.core.domain.datasource.PreferencesDataSource
 import com.droidcon.simplejokes.core.presentation.Localization
 import com.droidcon.simplejokes.core.presentation.utils.SetSystemBarAppearance
+import com.droidcon.simplejokes.di.databaseModule
+import com.droidcon.simplejokes.di.jokesRepositoryModule
+import com.droidcon.simplejokes.di.localizationModule
+import com.droidcon.simplejokes.di.networkModule
+import com.droidcon.simplejokes.di.preferencesModule
+import com.droidcon.simplejokes.di.vaultModule
+import com.droidcon.simplejokes.di.viewModelsModule
 import com.droidcon.simplejokes.ui.theme.SimpleJokesTheme
 import kotlinx.coroutines.flow.distinctUntilChanged
+import org.koin.android.ext.koin.androidContext
+import org.koin.compose.KoinApplication
 import org.koin.compose.koinInject
 
 class MainActivity : AppCompatActivity() {
@@ -22,28 +31,42 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
 
         setContent {
-            val preferencesDataSource = koinInject<PreferencesDataSource>()
-            val localization = koinInject<Localization>()
+            KoinApplication(application = {
+                androidContext(this@MainActivity.applicationContext)
+                modules(
+                    databaseModule,
+                    jokesRepositoryModule,
+                    localizationModule,
+                    networkModule,
+                    preferencesModule,
+                    vaultModule,
+                    viewModelsModule
+                )
+            }) {
+                val preferencesDataSource = koinInject<PreferencesDataSource>()
+                val localization = koinInject<Localization>()
 
-            val themePreference by preferencesDataSource.getTheme().collectAsStateWithLifecycle("")
-            val useDarkTheme = when (themePreference) {
-                "DARK" -> true
-                "LIGHT" -> false
-                else -> isSystemInDarkTheme() // Default to system theme
-            }
-
-            SimpleJokesTheme(darkTheme = useDarkTheme) {
-                SetSystemBarAppearance(darkTheme = useDarkTheme)
-
-                LaunchedEffect(Unit) {
-                    preferencesDataSource.getLanguage()
-                        .distinctUntilChanged()
-                        .collect { savedLanguageTag ->
-                            localization.updateLocale(savedLanguageTag)
-                        }
+                val themePreference by preferencesDataSource.getTheme()
+                    .collectAsStateWithLifecycle("")
+                val useDarkTheme = when (themePreference) {
+                    "DARK" -> true
+                    "LIGHT" -> false
+                    else -> isSystemInDarkTheme() // Default to system theme
                 }
 
-                NavigationRoot()
+                SimpleJokesTheme(darkTheme = useDarkTheme) {
+                    SetSystemBarAppearance(darkTheme = useDarkTheme)
+
+                    LaunchedEffect(Unit) {
+                        preferencesDataSource.getLanguage()
+                            .distinctUntilChanged()
+                            .collect { savedLanguageTag ->
+                                localization.updateLocale(savedLanguageTag)
+                            }
+                    }
+
+                    NavigationRoot()
+                }
             }
         }
     }
