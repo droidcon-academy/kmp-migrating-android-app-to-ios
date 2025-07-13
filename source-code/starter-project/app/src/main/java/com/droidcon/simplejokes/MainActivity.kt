d434caba7965af3bcccce546635f9cbddfb135bc
ioannisa <ioannisanif@gmail.com>
2025-07-14 00:29:02 +0300

    JokesThemeManager

--- source-code/starter-project/app/src/main/java/com/droidcon/simplejokes/MainActivity.kt
+++ source-code/starter-project/app/src/main/java/com/droidcon/simplejokes/MainActivity.kt
@@ -6,12 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -21,6 +18,7 @@ import com.droidcon.simplejokes.core.domain.datasource.PreferencesDataSource
 import com.droidcon.simplejokes.core.presentation.Localization
 import com.droidcon.simplejokes.core.presentation.SetSystemBarAppearance
 import com.droidcon.simplejokes.core.presentation.SnackbarManager
+import com.droidcon.simplejokes.core.presentation.ui.theme.SimpleJokesTheme
 import dagger.hilt.android.AndroidEntryPoint
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.distinctUntilChanged
@@ -44,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
 
         setContent {
-            JokesAppTheme(
+            JokesThemeManager(
                 preferencesDataSource = preferencesDataSource,
             ) {
                 AppEffectHost(
@@ -64,7 +62,7 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-private fun JokesAppTheme(preferencesDataSource: PreferencesDataSource, content: @Composable () -> Unit) {
+private fun JokesThemeManager(preferencesDataSource: PreferencesDataSource, content: @Composable () -> Unit) {
     val themePreference by preferencesDataSource.getTheme().collectAsStateWithLifecycle("")
     val systemIsDark = isSystemInDarkTheme()
 
@@ -76,7 +74,7 @@ private fun JokesAppTheme(preferencesDataSource: PreferencesDataSource, content:
         }
     }
 
-    MaterialTheme(colorScheme = if (useDarkTheme) darkColorScheme() else lightColorScheme()) {
+    SimpleJokesTheme(darkTheme = useDarkTheme) {
         SetSystemBarAppearance(darkTheme = useDarkTheme)
         content() // Render the rest of the app inside the theme
     }
